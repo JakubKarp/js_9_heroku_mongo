dfe212f3abde5708ea12dc1fdd83257cc6dac3af
JakubKarp <jakub.karp@innespacery.pl>
2018-01-29 19:02:10 +0100

    heroku6

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs')
 
 
